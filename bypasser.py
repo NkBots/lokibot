ec69d75959f5cdf153cacc9ad8593d374254ca99
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-11 18:08:41 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -179,7 +179,46 @@ def psa_bypasser(psa_url):
 
 
 ##################################################################################################################
-# rocklinks
+ 
+
+url = "https://link.tnlink.in/TS95D7"  #@param {type:"string"}
+
+
+def tnlink(url):
+    
+    client = requests.session()
+    
+    
+    DOMAIN = "https://gadgets.usanewstoday.club"
+
+    url = url[:-1] if url[-1] == '/' else url
+
+    code = url.split("/")[-1]
+    
+    final_url = f"{DOMAIN}/{code}"
+    
+    ref = "https://usanewstoday.club/"
+    
+    h = {"referer": ref}
+  
+    resp = client.get(final_url,headers=h)
+    
+    soup = BeautifulSoup(resp.content, "html.parser")
+    
+    inputs = soup.find_all("input")
+   
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = { "x-requested-with": "XMLHttpRequest" }
+    
+    time.sleep(8)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
+    
+######
+
 
 def rocklinks(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
@@ -1757,6 +1796,9 @@ def shortners(url):
     elif "https://adrinolinks." in url:
         print("entered adrinolink:",url)
         return adrinolink(url)
+    elif "https://link.tnlink" in url:
+        print("entered linktnkink:",url)
+        return tnlink(url)
         
     # htpmovies sharespark cinevood
     elif "https://htpmovies." in url or 'https://sharespark.me/' in url or "https://cinevood." in url or "https://atishmkv." in url \
